d5242cc3f534b0cfcc66fe1fab2e306a2df60cef
Araceli Lasso <lassoaraceli@gmail.com>
2025-01-04 18:16:32 -0300

    agregando fondo y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,17 @@ import './App.css'
 function App() {
 
   return (
-  <div>App</div>
+    <div className="background-container">
+    <div className="content">
+      <h2>Envíale la carta a Papá Noel</h2>
+      <p class="mensaje-navidad">¡Feliz Navidad!</p>
+    </div>
+    
+    {/* Crédito al autor del patrón */}
+    <p className="credit">
+      Background by <a href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/">Free SVG Backgrounds and Patterns by SVGBackgrounds.com</a>
+    </p>
+  </div>
   )
 }
 
