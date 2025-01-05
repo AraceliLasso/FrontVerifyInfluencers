a11bcd0f47a4fa96cc4f97c4f81a2c7e1b5573e4
Araceli Lasso <lassoaraceli@gmail.com>
2025-01-05 15:48:47 -0300

    agregando/modificando inputs de formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -10,9 +10,32 @@ const Formulario = () => {
             <label htmlFor="edad">Edad</label>
             <input type="number" />
 
-            {/* Pais */}
+            {/* País */}
             <label htmlFor="pais">País</label>
-            <input type="text" />
+            <select>
+                <option value="ar">Argentina</option>
+                <option value="bo">Bolivia</option>
+                <option value="br">Brasil</option>
+                <option value="cb">Cuba</option>
+                <option value="cl">Chile</option>
+                <option value="cm">Colombia</option>
+                <option value="cr">Costa Rica</option>
+                <option value="do">República Dominicana</option>
+                <option value="ec">Ecuador</option>
+                <option value="gt">Guatemala</option>
+                <option value="hn">Honduras</option>
+                <option value="ht">Haití</option>
+                <option value="mx">México</option>
+                <option value="ni">Nicaragua</option>
+                <option value="pa">Panamá</option>
+                <option value="pe">Perú</option>
+                <option value="pr">Paraguay</option>
+                <option value="prico">Puerto Rico</option>
+                <option value="sal">El Salvador</option>
+                <option value="uy">Uruguay</option>
+                <option value="ve">Venezuela</option>
+            </select>
+
 
             {/* correo */}
             <label htmlFor="correo">Correo</label>
@@ -29,6 +52,11 @@ const Formulario = () => {
             {/* Deseo */}
             <label htmlFor="deseo">Cuéntame qué quieres para Navidad:</label>
             <textarea id="deseo" rows="5" cols="30" placeholder="Escribe aquí tu deseo..." />
+
+            {/* Imagen */}
+            <p>¿Tienes una imagen de lo que quieres?</p>
+            <label htmlFor="imagen">¡Maravilloso! Muestrame</label>
+            <input type="file"/>
 
             <button type="submit">Enviar</button>
         </form>
